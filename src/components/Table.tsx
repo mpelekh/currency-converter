29750994e0bca58ee3941cfee32c747de06de775
Mykola Pelekh <nick.peleh@gmail.com>
2023-02-02 23:18:28 +0200

    Add Table component

---
+++ src/components/Table.tsx
@@ -0,0 +1,32 @@
+import React, {PropsWithChildren} from 'react';
+import BootstrapTable from 'react-bootstrap/Table'
+
+interface Props<ObjectType> {
+	items: ObjectType[];
+	columns: {
+		key: keyof ObjectType & string,
+		title: string,
+	}[];
+}
+
+
+function Table<ObjectType extends { id: number }>(
+	{items, columns}: PropsWithChildren<Props<ObjectType>>,
+) {
+	return (
+		<BootstrapTable striped>
+			<thead>
+			<tr>
+				{columns.map(column => (<th key={column.key}>{column.title}</th>))}
+			</tr>
+			</thead>
+			<tbody>
+			{items.map((item => (<tr key={item.id}>
+				{columns.map(column => (<td key={column.key}>{item[column.key as string]}</td>))}
+			</tr>)))}
+			</tbody>
+		</BootstrapTable>
+	);
+}
+
+export default Table;
